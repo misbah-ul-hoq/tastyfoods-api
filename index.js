2e28a725fd8e45415a79763ce4a4a3eca8a76b7d
Misbah Ul Hoq <extraordinarymisbah@gmail.com>
2024-07-15 22:43:24 +0600

    created menu and reviews get api

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const cors = require("cors");
+const port = process.env.PORT || 8080;
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5dbzkti.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const menu = client.db("tastyFoodsDb").collection("menu");
+    const reviews = client.db("tastyFoodsDb").collection("reviews");
+
+    app.get("/menu", async (req, res) => {
+      const result = await menu.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/reviews", async (req, res) => {
+      const result = await reviews.find().toArray();
+      res.send(result);
+    });
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Restaurant server is running ");
+});
+
+app.listen(port, () => {
+  console.log(`server started at port http://localhost:${port}`);
+});
